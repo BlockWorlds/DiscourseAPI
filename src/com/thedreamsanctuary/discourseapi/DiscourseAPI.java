2d9155096a88a7cec10c6143b26afedbe67d5968
Shiggysama <ctbuschmann@gmail.com>
2014-08-07 20:27:52 +0200

    Fixed Initialization Issue

--- src/com/thedreamsanctuary/discourseapi/DiscourseAPI.java
+++ src/com/thedreamsanctuary/discourseapi/DiscourseAPI.java
@@ -5,16 +5,9 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.wareninja.opensource.discourse.DiscourseApiClient;
-import com.wareninja.opensource.discourse.utils.ResponseListener;
-import com.wareninja.opensource.discourse.utils.ResponseMeta;
-import com.wareninja.opensource.discourse.utils.ResponseModel;
 
 public class DiscourseAPI extends JavaPlugin{
 	public void onEnable(){
@@ -70,29 +63,9 @@ public class DiscourseAPI extends JavaPlugin{
                     + " to system classloader");
         }
     }
-	private DiscourseApiClient apiclient;
-	private ResponseListener response;
-	
-	public void initiate(String api_url, String api_key, String api_username){
-		apiclient = new DiscourseApiClient(api_url, api_key, api_username);
-		response = new ResponseListener(){
-
-			@Override
-			public void onBegin(String info) {
-				//System.out.println("info: "+info);
-			}
-			@Override
-			public void onComplete_wModel(ResponseModel responseModel) {
-				// successful result
-				//System.out.println("SUCCESS! -> " + responseModel.toString());
-			}
-
-			@Override
-			public void onError_wMeta(ResponseMeta responseMeta) {
-				// error
-				System.out.println("ERROR! -> " + responseMeta.toString());
-			}
-		};
+	private static Handler handler;
+	public static void initiate(String api_url, String api_key, String api_username){
+		handler = new Handler(api_url,api_key,api_username);
 	}
 	/**
 	 * Creates a new topic
@@ -100,12 +73,8 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param topic_content Body of the topic, as html
 	 * @param topic_category Category of the topic, case-sensitive
 	 */
-	public void createTopic(String topic_title, String topic_content, String topic_category){
-		Map<String,String> data = new HashMap<String, String>();
-		data.put("title", topic_title);
-		data.put("raw", topic_content);
-		data.put("category", topic_category);
-		apiclient.createTopic(data,response);
+	public static void createTopic(String topic_title, String topic_content, String topic_category){
+		handler.createTopic(topic_title, topic_content, topic_category);
 	}
 	
 	/**
@@ -115,13 +84,7 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param username
 	 * @param password
 	 */
-	public void createUser(String name, String email, String username, String password){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("name", name);
-		data.put("email", email);
-		data.put("username", username);
-		data.put("password", password);
-		apiclient.createUser(data,response);
+	public static void createUser(String name, String email, String username, String password){
 		
 	}
 	
@@ -130,11 +93,7 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param userid
 	 * @param username
 	 */
-	public void approveUser(String userid,String username){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("userid", userid);
-		data.put("username", username);
-		apiclient.approveUser(data,response);
+	public static void approveUser(String userid,String username){
 	}
 	
 	/**
@@ -142,11 +101,7 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param userid
 	 * @param username
 	 */
-	public void activateUser(String userid, String username){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("userid", userid);
-		data.put("username", username);
-		apiclient.activateUser(data,response);
+	public static void activateUser(String userid, String username){
 	}
 	
 	/**
@@ -154,21 +109,14 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param userid
 	 * @param level 0 (new user), 1 (basic user), 2 (regular user), 3 (leader), 4 (elder)
 	 */
-	public void trustUser(String userid,int level){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("userid", userid);
-		data.put("level", ""+level);
-		apiclient.trustUser(data, response);
+	public static void trustUser(String userid,int level){
 	}
 	
 	/**
 	 * Generates an API key for the specified user
 	 * @param userid
 	 */
-	public void generateApiKey(String userid){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("userid", userid);
-		apiclient.generateApiKey(data,response);
+	public static void generateApiKey(String userid){
 	}
 	
 	/**
@@ -176,11 +124,7 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param userid
 	 * @param username
 	 */
-	public void deleteUser(String userid, String username){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("userid", userid);
-		data.put("username", username);
-		apiclient.deleteUser(data,response);
+	public static void deleteUser(String userid, String username){
 	}
 	
 	/**
@@ -188,20 +132,13 @@ public class DiscourseAPI extends JavaPlugin{
 	 * @param username
 	 * @param password
 	 */
-	public void loginUser(String username, String password){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("username", username);
-		data.put("password", password);
-		apiclient.deleteUser(data,response);
+	public static void loginUser(String username, String password){
 	}
 	
 	/**
 	 * Logs the specified user out
 	 * @param username
 	 */
-	public void logoutUser(String username){
-		Map<String,String> data = new HashMap<String,String>();
-		data.put("username", username);
-		apiclient.logoutUser(data, response);
+	public static void logoutUser(String username){
 	}
 }
